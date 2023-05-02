2a62b9ea5b6da77920e6e4885972a31e8d06af63
motebaya <davinsexo722@gmail.com>
2023-05-02 09:43:45 +0700

    :add config

--- lib/savetube_class.rb
+++ lib/savetube_class.rb
@@ -2,7 +2,7 @@
 # Tue June 10 10:44:16 2022
 # from ytshorts.savetube.me cli/module version
 # © Copyright 2022.06 github.com/motebaya
-# update : Thu Apr 27 02:30:18 PM 2023
+# update : tue may.02.2023 09:42:26 AM
 
 class SaveTube < Helper
 
@@ -13,9 +13,16 @@ class SaveTube < Helper
         - formats (str): audio or video
         - output (str): output path location to save, default: current dir
     """
-    def initialize(url=nil, formats=nil, path=nil, cli=false)
+    def initialize(
+        url=nil, 
+        formats=nil, 
+        path=nil, 
+        cli=false, 
+        default_quality=nil
+    )
         super()
         @shortUrl = url
+        @default_quality = default_quality
         @host = "https://ytshorts.savetube.me/"
         @api = "https://cdn69.savetube.me"
         @headers = 	{
@@ -27,8 +34,8 @@ class SaveTube < Helper
         }
         @output = getOutput(path)
         @cli = cli
-        @formats = !formats.nil? ? formats : 'video'
-        @shortId = url.match(%r"shorts\/([\w]+)").to_s
+        @formats = !formats.nil? ? formats : 'video' # the default formats is video
+        @shortId = check_url(@shortUrl)[1] #url.match(%r"shorts\/([\w]+)").to_s
     end
 
     def analyze
@@ -36,6 +43,7 @@ class SaveTube < Helper
         js = loadJson(page.to_s)
         if (js["status"] and js['message'].to_i == 200)
             if (["audio","video"].include?(@formats))
+                # extract the info
                 response = {
                     "key" => js['data']['key'],
                     "title" => js['data']['title'],
@@ -66,21 +74,42 @@ class SaveTube < Helper
         if (!quality && !response)
             logger("savetube", "downloading webpage: #{@shortId}")
             data = analyze
+            # puts data
             if (!data.nil?)
                 logger("savetube", "Title: #{data['title']}")
                 logger("savetube", "list quality:")
                 puts
+
+                dformats = nil
                 data["res"].each_with_index do | label, index |
                     printf(
-                        " (%d). %s\n", index + 1, label
+                        " \033[1;32m[\033[0m%02d\033[1;32m]\033[0m. %s\n", index + 1, label
                     )
+                    # check default quality set
+                    if (dformats.nil? && !@default_quality.nil?)
+                        if label.include?(@default_quality)
+                            dformats = index + 1
+                        end
+                    end
                 end
                 puts
-                quality = prompt(" (savetube) Set : ").to_i
-                if (quality - 1) < data['res'].length
-                    res = data['res'][quality - 1].split('->')[0].strip
+
+                if dformats.nil?
+                    if @default_quality.nil?
+                        dformats = prompt(" \033[1;32m[\033[0msavetube\033[1;32m]\033[0m Set : ").to_i
+                    else
+                        logger("savetube", "skipped: nothing `#{@defaul_quality}` for #{@shortId}.")
+                        return
+                    end
+                end
+
+                if (dformats - 1) < data['res'].length
+                    choicedFormats = data['res'][dformats - 1]
+                    logger("savetube", "converting to: #{choicedFormats}")
+                    res = choicedFormats.split('->')[0].strip
                     filename = data['title'] + (@formats == 'audio' ? '.mp3' : '.mp4')
                     logger("savetube", "downloading: #{filename}")
+                    # start converting
                     get_link = loadJson(getPage("#{@api}/download/#{@formats}/#{res}/#{data['key']}", headers=@headers).to_s)
                     if (get_link['status'] and get_link['message'].to_i == 200)
                         download_stream(
@@ -88,7 +117,8 @@ class SaveTube < Helper
                             filename,
                             @output
                         )
-                        logger("savetube", "file saved in: #{@output}#{filename}")
+                        logger("savetube", "saved as: #{@output}#{filename}")
+                        puts
                     else
                         abort(data.to_s)
                     end
@@ -99,6 +129,7 @@ class SaveTube < Helper
                 abort(data.to_s)
             end
         else
+            # return as JSON string if set @cli to false
             response = loadJson(response)
             if (response[:res].map { |qual| qual.split('->')[0].strip}).include?(quality.to_s)
                 jResponse = loadJson(
@@ -113,9 +144,10 @@ class SaveTube < Helper
                     return JSON.dump(jResponse)
                 end
             else
-                abort("invalid quality")
+                return JSON.dump({
+                    "error": "invalid quality"
+                })
             end
         end
     end
 end
-
